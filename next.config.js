6488bda3dae087c1d0ddaa1262c3e29fadae29f1
PPaliagkas <pantelis.paliagkas@medable.com>
2023-01-20 01:29:20 +0400

    Revert referrer policies.

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: "/:path*",
-        headers: [
-          {
-            key: "Referrer-Policy",
-            value: "origin",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
